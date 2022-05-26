90623bb2b1f74caabec16e8efb3d111fae981bf8
arunandroid <arun.kumer@pvtltd.com>
2022-05-26 13:07:10 +0530

    update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -19,46 +19,75 @@ class _HomePagesState extends State<HomePages> {
               Navigator.pop(context, true);
             }),
         actions: <Widget>[
-          IconButton(onPressed: (){
-
-          }, icon:  Icon(Icons.notifications),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications),
             tooltip: "Save Todo and Retrun to List",
           ),
-
         ],
       ),
       body: Center(
         child: GridView.count(
           crossAxisCount: 3,
-          children: List.generate(10, (index) => _myCardItem(context)),
+          children: List.generate(9, (index) => _myCardItem(context,index)),
         ),
       ),
     );
   }
 }
 
-Widget _myCardItem(BuildContext context) {
-  return Card(
-    elevation: 2,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(25.0),
-    ),
-    child: Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        boxShadow: const [
-          BoxShadow(
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(0, 3), // changes position of shadow
-          )
-        ],
-        image: const DecorationImage(
-          image: NetworkImage(
-              "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467__340.jpg"),
-          fit: BoxFit.cover,
+Widget _myCardItem(BuildContext context, int index) {
+
+  return Padding(
+    padding:  const EdgeInsets.all(8.0),
+    child: InkWell(
+      splashColor: Colors.blueAccent,
+      onTap: () {
+        ScaffoldMessenger.of(context).showSnackBar( const SnackBar(
+          content: Text("Clicked"),
+        ));
+      },
+      child: Stack(
+        children: [Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            image: const DecorationImage(
+              image: AssetImage("assets/images/image3.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Colors.black54,
+            ),
+
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:  [
+                const Icon(
+                  Icons.play_arrow,
+                  color: Colors.white,
+                  size: 30,
+                ),
+                Text(
+                  "Button #$index",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16),
+                )
+              ],
+            ),
+          ),
+      ],
+
+      )
     ),
   );
 }
