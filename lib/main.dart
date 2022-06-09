0045e9e70e2dba2ffba6be22e212006e5f82c734
arunandroid <arun.kumer@pvtltd.com>
2022-06-09 16:44:51 +0530

    use card

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_navigation/screen/Example0.dart';
 import 'package:flutter_navigation/screen/Example1.dart';
--- lib/screen/Example3.dart
+++ lib/screen/Example3.dart
@@ -13,13 +13,14 @@ class Example3 extends StatefulWidget {
 
 class _ExampleState extends State<Example3> {
   late Future<List<Character>> characterList;
+  late Future<List<Posts>> postsList;
+
   @override
   void initState() {
     super.initState();
     characterList = fetchData();
+    postsList = fetchPosts();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -33,30 +34,70 @@ class _ExampleState extends State<Example3> {
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     List<Character>? data = snapshot.data;
-
                     return ListView.builder(
                         itemCount: data?.length,
                         itemBuilder: (context, index) {
-                          return ListTile(
-                            title: Text(data![index].name),
-                            subtitle: Text(data[index].nickname),
-                            leading: CircleAvatar(
-                              backgroundImage:
-                              NetworkImage(data[index].img),
-                            ),
-                          );
+                          return _buildContainer(data![index]);
                         });
                   } else if (snapshot.hasError) {
                     return Text('${snapshot.error}');
                   }
                   return const CircularProgressIndicator();
-                }))
+                })));
+  }
+
+  Widget _buildContainer(Character data) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: InkWell(
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(data.img),
+                backgroundColor: Colors.greenAccent,
+                radius: 40,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      data.name,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: Colors.black),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      data.nickname,
+                      style: const TextStyle(fontSize: 15, color: Colors.black26),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      elevation: 5,
+      shadowColor: Colors.greenAccent,
+      shape: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: Colors.white10)),
     );
   }
 
   Future<List<Character>> fetchData() async {
     final response =
-    await http.get(Uri.parse("https://breakingbadapi.com/api/characters"));
+        await http.get(Uri.parse("https://breakingbadapi.com/api/characters"));
     if (response.statusCode == 200) {
       List jsonResponse = jsonDecode(response.body);
       return jsonResponse.map((data) => Character.fromJson(data)).toList();
@@ -67,8 +108,45 @@ class _ExampleState extends State<Example3> {
     }
   }
 
-
+  Future<List<Posts>> fetchPosts() async {
+    final response =
+        await http.get(Uri.parse("https://jsonplaceholder.typicode.com/posts"));
+    if (response.statusCode == 200) {
+      List jsonResponse = jsonDecode(response.body);
+      return jsonResponse.map((data) => Posts.fromJson(data)).toList();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
 }
+
+class Posts {
+  int? userId;
+  int? id;
+  String? title;
+  String? body;
+
+  Posts({this.userId, this.id, this.title, this.body});
+
+  Posts.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['userId'] = this.userId;
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['body'] = this.body;
+    return data;
+  }
+}
+
 class Character {
   final int id;
   final String name;
